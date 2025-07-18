ddaccff37b9bba02439871e64f59185e7a025cc5
知式 <86602533+typezhi@users.noreply.github.com>
2025-07-18 21:43:50 +0800

    Update ksu.c

--- drivers/staging/ksu/ksu.c
+++ drivers/staging/ksu/ksu.c
@@ -86,5 +86,5 @@ MODULE_DESCRIPTION("Android KernelSU");
 
 // 只有在新内核才引入该 namespace，否则会导致语法错误
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
-MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+//MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 #endif
